bb2ee0d6277ae98b09be36d491b2eb1b364e83b3
pjustus <justus.poutanen@helsinki.fi>
2021-01-17 23:15:23 +0200

    AccessViz script

--- AccessViz.py
+++ AccessViz.py
@@ -50,13 +50,15 @@ License:
     # Defining starting screen
     print("Welcome to AccessViz. Press enter at any time to exit.")
     # Asking if user wants to see YKR ID grid map
-    map_check = av.yn_loop("Do you want to see a map with the YKR ID grid? yes/no: ", yn_list)
+    map_check = yn_loop("Do you want to see a map with the YKR ID grid? yes/no: ", yn_list)
     if map_check == "yes":
         YKR_map()
     elif map_check == "":
         return print(exit)
     # Asking for user's list of YKR ID's
     YKR_IDs_input = input("Insert a list of YKR ID's (e.g. [1234567, 5787544, 9876543]): ")
+    if YKR_IDs_input == "":
+        return print(exit)
     # Converting the input to a list
     list1 = YKR_IDs_input[1:]
     list2 = [str(x) for x in list1.split()]
@@ -65,43 +67,57 @@ License:
         i = i[:-1]
         YKR_IDs.append(i)
     # Asking if user wants to extract files
-    zip_check = av.yn_loop("Do you need to extract the TimeTravelMatrix (TTM) files n\ or the MetropAccess_YKR_grid? yes/no: ", yn_list)
+    zip_check = yn_loop("Do you need to extract the TimeTravelMatrix (TTM) files n\ or the MetropAccess_YKR_grid? yes/no: ", yn_list)
     if zip_check == "yes":
         zip_name = input("Insert the name of the zip file: ")
+        if zip_name == "":
+            return print(exit)
         zip_target = input("Insert the name of the target folder: ")
-        av.unzip(zip_name, zip_target)
+        if zip_target == "":
+            return print(exit)
+        unzip(zip_name, zip_target)
         return print("Please restart AccessViz.")
     elif zip_check == "":
         return print(exit)
     # Asking for user's input folder
     input_folder_name = input("Insert the name of the folder where the TTM files are: ")
+    if input_folder_name == "":
+        return print(exit)
     # Asking for user's output folder
     output_folder_name = input("Insert the name of the output folder: ")
+    if output_folder_name == "":
+        return print(exit)
     # Asking if user wants the TTM filepaths to a text file
-    FFF_check = av.yn_loop("TTM filepaths to a text file? yes/no: ", yn_list)
+    FFF_check = yn_loop("TTM filepaths to a text file? yes/no: ", yn_list)
     if FFF_check == "yes":
-        FF_result = av.FileFinder(YKR_IDs, input_folder_name, output_folder_name, to_file=True)
+        FF_result = FileFinder(YKR_IDs, input_folder_name, output_folder_name, to_file=True)
     elif FFF_check == "no":
-        FF_result = av.FileFinder(YKR_IDs, input_folder_name, output_folder_name)
+        FF_result = FileFinder(YKR_IDs, input_folder_name, output_folder_name)
     elif FFF_check == "":
         return print(exit)
     # Creating and getting filepaths of geopackage files
-    TJ_result = av.TableJoiner(YKR_IDs, output_folder_name, FF_result)
+    TJ_result = TableJoiner(YKR_IDs, output_folder_name, FF_result)
     # Asking if user wants to visualize results
-    visu_check = av.yn_loop("Do you want to visualize the results? yes/no: ", yn_list)
+    visu_check = yn_loop("Do you want to visualize the results? yes/no: ", yn_list)
     if visu_check == "yes":
         visualizer_tm = input("Insert the mode of travel for Visualizer, car/pt/bike/walk: ")
+        if visualizer_tm == "":
+            return print(exit)
         visualizer_mp = input("Insert the type of map for Visualizer, static/interactive: ")
-        av.Visualizer(YKR_IDs, output_folder_name, visualizer_tm, visualizer_mp, TJ_result)
+        if visualizer_mp == "":
+            return print(exit)
+        Visualizer(YKR_IDs, output_folder_name, visualizer_tm, visualizer_mp, TJ_result)
         print(f"The map(s) can be found in: {output_folder_name}")
     elif visu_check == "no":
         print(f"Geopackage files can be found in: {output_folder_name}")
     elif visu_check == "":
         return print(exit)
     # Asking if user wants to compare travel modes
-    ctool_check = av.yn_loop("Do you want to compare two travel modes? yes/no: ", yn_list)     
+    ctool_check = yn_loop("Do you want to compare two travel modes? yes/no: ", yn_list)     
     if ctool_check == "yes":
         tm_input = input("Insert a list of travel modes to be compared (e.g. [car, pt]): ")
+        if tm_input == "":
+            return print(exit)
         # Converting the input to a list
         tm_list1 = tm_input[1:]
         tm_list2 = [str(x) for x in tm_list1.split()]
@@ -109,7 +125,7 @@ License:
         for i in tm_list2:
             i = i[:-1]
             tms.append(i)
-        av.ComparisonTool(tms, TJ_result, output_folder_name)
+        ComparisonTool(tms, TJ_result, output_folder_name)
         return print(exit)
     else:
         return print(exit)
